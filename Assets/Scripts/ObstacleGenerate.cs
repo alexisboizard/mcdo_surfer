b3aae42d89d700bdf568899481699f68ab5d2bb6
VAITON DAMIEN p2106425 <damien.vaiton@etu.univ-lyon1.fr>
2024-01-31 15:42:11 +0100

    obstacles ok (no cars)

--- Assets/Scripts/ObstacleGenerate.cs
+++ Assets/Scripts/ObstacleGenerate.cs
@@ -9,15 +9,33 @@ public class ObstacleGenerate : MonoBehaviour
     public GameObject[] prefabsObstacle;
     public GameObject player;
     [SerializeField] public GameObject gameOverPanel;
+    private Vector3 spawnpoint;
     private int nbLife = 3;
-    private bool isGameOver = false;
+    private int desiredLanespawn;
 
 
 
     void Start()
     {
         gameOverPanel.SetActive(false);
+        ScheduleRandomObstacle();
     }
+
+
+    private void ScheduleRandomObstacle()
+    {
+        float randomDelay = Random.Range(0.1f, 2.0f); // Choisir une plage de délais aléatoires
+        Debug.Log("Next Obstacle delay in " + randomDelay + " seconds.");
+        Invoke("DelayOfObstacle", randomDelay);
+    }
+
+
+    private void DelayOfObstacle()
+    {
+        GenerateObstacle(); 
+        ScheduleRandomObstacle(); 
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
@@ -32,7 +50,19 @@ public class ObstacleGenerate : MonoBehaviour
 
     private void GenerateObstacle()
     {
-        
+        desiredLanespawn = Random.Range(0, 3);
+        float xcoodonate = transform.position.x;
+        float ycoodonate = transform.position.y;
+        float zcoodonate = transform.position.z;
+        float[] spwnco = { -3.25f, 0.25f, 3.25f };
+
+       
+            int randomIndex = Random.Range(0, prefabsObstacle.Length);
+            GameObject obstacle = prefabsObstacle[randomIndex];
+            spawnpoint = new Vector3(xcoodonate - 100f, 0.686801f, spwnco[desiredLanespawn]);
+            Instantiate(obstacle, spawnpoint, Quaternion.identity);
+       
+
     }
 
     private void DestroyPlayer()
